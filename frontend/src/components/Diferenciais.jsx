8f34b5274c973df6e306b87c10af8ac3ac908f95
noahdondacasagrande-glitch <noahdondacasagrande@gmail.com>
2026-08-19 12:39:51 -0300

    Add files via upload

---
+++ frontend/src/components/Diferenciais.jsx
@@ -0,0 +1,47 @@
+import tomateImg from '../assets/tomateIMG';
+import fogoImg from '../assets/fogoIMG';
+import queijoImg from '../assets/queijo.png';
+import relampagoImg from '../assets/relampago.png'
+
+const listaDiferenciais = [
+    {
+        id: 1,
+        imagem: tomateImg,
+        alt: "Tomate",
+        texto: "algo vermelho"
+    },
+    {
+        id: 2,
+        imagem: fogoImg,
+        alt: "fogo",
+        texto: "pegando fogo"
+    },
+    {
+        id: 3,
+        imagem: queijoImg,
+        alt: "quijo",
+        texto: "o rato come"
+    },
+    {
+        id: 4,
+        imagem: relampagoImg,
+        alt: "relampago",
+        texto: "cabuuuummmmmmmmmmmm"
+    }   
+];
+
+export default function Diferenciais(){
+    return(
+        <section className='diferenciais' id='diferenciais'>
+            <h2> nossos diferenciais</h2>
+            <div className='cards'>
+                {listaDiferenciais.map((item) =>(
+                    <div className="card" key={item.id}>
+                        <img src={item.imagem} alt={item.alt} />
+                        <p>{item.texto}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
+}
