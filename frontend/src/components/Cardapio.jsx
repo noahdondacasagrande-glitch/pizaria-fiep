e57427b26c862d7c9dd8323e6bf2cc3aec1ab99a
noahdondacasagrande-glitch <noahdondacasagrande@gmail.com>
2026-08-20 11:16:22 -0300

    Add files via upload

---
+++ frontend/src/components/Cardapio.jsx
@@ -0,0 +1,21 @@
+export default function Cardapio(){
+    return(
+        <section className="cardapio" id="cardapio">
+            <h2>CArdapio</h2>
+            <ul data-title="🍕 Tradicionais">
+                <li> </li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
+    )
+
+}
